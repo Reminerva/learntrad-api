85c6df4e7488759eef6f75ddf0f75f50bb8bae7a
Reksa Alamsyah <reksaalamsyah@gmail.com>
2025-08-01 19:30:51 +0700

    [FIX] fix bugs

--- notification-service/src/main/java/com/learntrad/microservices/notification/NotificationServiceApplication.java
+++ notification-service/src/main/java/com/learntrad/microservices/notification/NotificationServiceApplication.java
@@ -13,6 +13,7 @@ public class NotificationServiceApplication {
         System.setProperty("MAIL_PORT", dotenv.get("MAIL_PORT"));
         System.setProperty("MAIL_USERNAME", dotenv.get("MAIL_USERNAME"));
         System.setProperty("MAIL_PASSWORD", dotenv.get("MAIL_PASSWORD"));
+        System.setProperty("MAIL_EMAIL", dotenv.get("MAIL_EMAIL"));
 		SpringApplication.run(NotificationServiceApplication.class, args);
 	}
 
